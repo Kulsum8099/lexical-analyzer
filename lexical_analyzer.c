73d8ed6cba7d107896dae43039db72bb02d4fe26
Umme Kulsum Suriya <121636815+Kulsum8099@users.noreply.github.com>
2024-11-25 00:07:27 +0600

    Update lexical_analyzer.c

--- lexical_analyzer.c
+++ lexical_analyzer.c
@@ -10,16 +10,19 @@ typedef enum {
     TOKEN_PARENTHESIS,
     TOKEN_MODULO,
     TOKEN_UNKNOWN
-} TokenType;
+} 
+TokenType;
 
 // Token structure
 typedef struct {
     TokenType type;
     char value[50];
-} Token;
+} 
+Token;
 
 // Function to classify characters
-TokenType classify_character(char c) {
+TokenType classify_character(char c)
+{
     if (isdigit(c)) return TOKEN_NUMBER;
     if (isalpha(c)) return TOKEN_IDENTIFIER;
     if (c == '+' || c == '-' || c == '*' || c == '/') return TOKEN_OPERATOR;
@@ -29,24 +32,30 @@ TokenType classify_character(char c) {
 }
 
 // Function to tokenize input
-void tokenize_expression(const char *expression) {
+void tokenize_expression(const char *expression) 
+{
     int i = 0, j = 0;
     char current_token[50] = {0};
 
-    while (expression[i] != '\0') {
+    while (expression[i] != '\0') 
+    {
         char c = expression[i];
         TokenType type = classify_character(c);
         
-        if (type == TOKEN_NUMBER || type == TOKEN_IDENTIFIER) {
+        if (type == TOKEN_NUMBER || type == TOKEN_IDENTIFIER) 
+        {
             current_token[j++] = c;
-        } else {
-            if (j > 0) { // End of a token
+        } 
+        else {
+            if (j > 0) 
+            { // End of a token
                 current_token[j] = '\0';
                 printf("Token: %-10s Type: %s\n", current_token, 
                        type == TOKEN_NUMBER ? "NUMBER" : "IDENTIFIER");
                 j = 0;
             }
-            if (type != TOKEN_UNKNOWN && !isspace(c)) {
+            if (type != TOKEN_UNKNOWN && !isspace(c)) 
+            {
                 printf("Token: %-10c Type: %s\n", c, 
                        type == TOKEN_OPERATOR ? "OPERATOR" :
                        type == TOKEN_PARENTHESIS ? "PARENTHESIS" : "MODULO");
@@ -61,7 +70,8 @@ void tokenize_expression(const char *expression) {
     }
 }
 
-int main() {
+int main() 
+{
     const char *expression = "(12 + x * (y - 25) / z) %15";
     printf("Analyzing Expression: %s\n", expression);
     tokenize_expression(expression);
